a9f84da3459ed944a20d550476bdc3df89e40944
FazioNico <dev-fazio@macbook-pro-de-dev-fazio.home>
2018-08-13 23:19:45 +0200

    upd(page): add service + DOM

--- src/app/features/page/containers/page/page.component.ts
+++ src/app/features/page/containers/page/page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+import { WpApiService } from '@app/shared/services';
 
 @Component({
   selector: 'app-page',
@@ -7,9 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageComponent implements OnInit {
 
-  constructor() { }
+  public data$: Observable<any>;
+
+  constructor(
+    private _router: Router,
+    private _wpApi: WpApiService
+  ) { }
 
   ngOnInit() {
+    this.data$ = this._wpApi.getData({path: 'pages', slug: `slug=${this._router.url.split('/').reverse()[0]}`}).pipe(
+      map(res => (res.length === 1 ) ? res[0] : res),
+      tap(data => (!data.type) ? window.location.href = '404' : null)
+    );
   }
 
 }
